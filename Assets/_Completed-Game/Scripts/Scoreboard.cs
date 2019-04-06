b9658eff41a512bf83d1035772be50045c2e2ca2
Chris CW Knight <chris@cwknight.com>
2019-04-06 09:36:58 -0700

    Pause, title screen, and inputs

--- Assets/_Completed-Game/Scripts/Scoreboard.cs
+++ Assets/_Completed-Game/Scripts/Scoreboard.cs
@@ -7,9 +7,10 @@ public class Scoreboard : MonoBehaviour
 {
     private int score;
     private int lives;
-    private string WIN_MESSAGE = "You Win!!";
+    private string WIN_MESSAGE = "You Win!! ";
     private string LOSE_MESSAGE = "You Lose!! :( ";
-    private string START_MESSAGE = "Press Any Button to Start";
+    private string START_MESSAGE = "Press Space/A to Start ";
+    private string PAUSE_MESSAGE = "Press Space/A to unpause ";
     private string LIVES_PREFIX = "Lives: ";
     private string SCORE_PREFIX = "Score: ";
     private enum STATE {Running, Over, Paused};
@@ -26,13 +27,29 @@ public class Scoreboard : MonoBehaviour
     void Start()
     {
         spawner = GetComponent<SpawnBall>();
-        NewGame();  
+        LoseGame();
+        messageText.text = START_MESSAGE;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        switch (currentState)
+        {
+            case STATE.Running:
+                if (Input.GetButtonUp("Cancel"))
+                {
+                    PauseGame();
+                }
+                break;
+            case STATE.Paused:
+            case STATE.Over:
+                if (Input.GetButtonUp("Submit"))
+                {
+                    UnPauseGame();
+                }
+                break;
+        }
     }
 
     public void NewGame()
@@ -42,6 +59,7 @@ public class Scoreboard : MonoBehaviour
         UpdateLivesText();
         UpdateScoreText();
         messageText.text = "";
+        Time.timeScale = 1.0f;
         currentState = STATE.Running;
         spawner.Spawn();
     }
@@ -49,14 +67,28 @@ public class Scoreboard : MonoBehaviour
     public void PauseGame()
     {
         currentState = STATE.Paused;
-        messageText.text = START_MESSAGE;
-        //need to implement pause. Possibly Time.timeScale = 0;
+        messageText.text = PAUSE_MESSAGE;
+        Time.timeScale = 0;
+    }
+
+    public void UnPauseGame()
+    {
+        if (currentState == STATE.Over)
+        {
+            NewGame();
+        }
+        else
+        {
+            currentState = STATE.Running;
+            messageText.text = "";
+            Time.timeScale = 1.0f;
+        }
     }
 
     public void WinGame()
     {
         Destroy(spawner.Ball);
-        messageText.text = WIN_MESSAGE;
+        messageText.text = WIN_MESSAGE + START_MESSAGE;
         currentState = STATE.Over;
     }
     public void LoseGame()
